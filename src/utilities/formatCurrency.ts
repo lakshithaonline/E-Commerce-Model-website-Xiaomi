d71f213a9ed8cc8408fb96965e6167d8ad193c67
Lakshitha Geethmal <dgtlgeethmal@students.nsbm.ac.lk>
2023-08-18 10:42:58 +0530

    navbar margin fix

--- src/utilities/formatCurrency.ts
+++ src/utilities/formatCurrency.ts
@@ -1,6 +1,7 @@
 const CURRENCY_FORMATTER = new Intl.NumberFormat('en-LK', {
   currency: 'LKR',
   style: 'currency',
+  minimumFractionDigits: 0, // Set this to 0 to remove cents
 });
 
 export function formatCurrency(number: number) {
