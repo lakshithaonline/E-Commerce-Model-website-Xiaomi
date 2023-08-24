3b56ee417f8d232226c00dc6aa7437f2fdfa7432
Lakshitha Geethmal <dgtlgeethmal@students.nsbm.ac.lk>
2023-08-24 15:36:23 +0530

    updates

--- src/components/Admin/settings.tsx
+++ src/components/Admin/settings.tsx
@@ -1,10 +1,95 @@
+import { useState, ChangeEvent } from 'react';
 import AdminNavigationBar from './AdminNavigationBar.tsx';
+
 function Settings() {
+    const [productData, setProductData] = useState({
+        id: 0,
+        name: '',
+        price: 0,
+        imgUrls: [],
+        category: '',
+    });
+    const [notification, setNotification] = useState('');
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setProductData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+    const addProduct = async () => {
+        try {
+            const response = await fetch('/api/products', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(productData),
+            });
+
+            if (response.ok) {
+                setNotification('Product added successfully');
+                setProductData({
+                    id: 0,
+                    name: '',
+                    price: 0,
+                    imgUrls: [],
+                    category: '',
+                });
+            } else {
+                const errorResponse = await response.json();
+                console.error('Failed to add product:', errorResponse);
+                setNotification('Failed to add product');
+            }
+        } catch (error) {
+            console.error('An error occurred', error);
+            setNotification('An error occurred');
+        }
+    };
+
     return (
         <div>
-            <AdminNavigationBar /> {/* Include the navigation bar */}
-            <h1>Settings</h1>
-            <p>Customize your application settings here.</p>
+            <AdminNavigationBar />
+            <h1>Add Product</h1>
+            <label>
+                Product Name:
+                <input
+                    type="text"
+                    name="name"
+                    value={productData.name}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <label>
+                Price:
+                <input
+                    type="number"
+                    name="price"
+                    value={productData.price}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <label>
+                Image URLs (comma-separated):
+                <input
+                    type="text"
+                    name="imgUrls"
+                    value={productData.imgUrls.join(',')}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <label>
+                Category:
+                <input
+                    type="text"
+                    name="category"
+                    value={productData.category}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <button onClick={addProduct}>Add Product</button>
+            <p>{notification}</p>
         </div>
     );
 }
---
+++ src/components/firebase.tsx
@@ -0,0 +1,19 @@
+/*
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBpiJWHY91-lrGuoHM13XADYtXzwn0YGOU",
+    authDomain: "xiaomi-e.firebaseapp.com",
+    databaseURL: "https://xiaomi-e-default-rtdb.firebaseio.com",
+    projectId: "xiaomi-e",
+    storageBucket: "xiaomi-e.appspot.com",
+    messagingSenderId: "739716171314",
+    appId: "1:739716171314:web:42afeb71eb8e153d2d9e2b"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.database();
+
+export default db;
+*/
